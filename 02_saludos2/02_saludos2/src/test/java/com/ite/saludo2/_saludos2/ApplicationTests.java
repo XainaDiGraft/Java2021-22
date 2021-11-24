6a58865665d99bb8165b3c12801aca1417a9a816
Olga Bueno Tamayo <58338238+XainaDiGraft@users.noreply.github.com>
2021-11-24 13:50:26 +0100

    Add files via upload

---
+++ 02_saludos2/02_saludos2/src/test/java/com/ite/saludo2/_saludos2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ite.saludo2._saludos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
